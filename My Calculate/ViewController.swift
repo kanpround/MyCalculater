a8095613c161836bd13566b27d865b73852fb23f
student09 <kannikar@snru.ac.th>
2017-06-13 10:28:09 +0700

    before morning brake 13 June 2017

--- My Calculate/ViewController.swift
+++ My Calculate/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+// decare. variable
+    //Emplicit ประกาศตัวแปร ที่แก้ไขได้
+    var intNumber1 = 0
+    var intNumber2: Int = 0
+    var strNumber1 = ""
+    var strNumber2: String = ""
+    
+    
+    @IBOutlet weak var Num1TextField: UITextField!
+    
+    
+    @IBOutlet weak var Num2TextField: UITextField!
+    
+    @IBAction func AnsButton(_ sender: Any) {
+        strNumber1 = Num1TextField.text!
+        strNumber2  =   Num2TextField.text!
+        //show log
+        print("strNumber1 ==>  \(strNumber1)")
+        print("strNumber2 ==>  \(strNumber2)")
+        //intNumber1 = convert (strNumber1)
+        
+       // print("Answer ==>  \Int(strNumber1)!+Int(strNuber2)!)")
+        
+    }   //Answer button
+    
+    
+    override func viewDidLoad() {       //method or function viewDidLoade()==> init every things
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   //end method or function viewDidLoade()
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {       //method or function didReceiveMemoryWarning()
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }   //end method or function didReceiveMemoryWarning()
 
 
-}
+}   // end main class
 
